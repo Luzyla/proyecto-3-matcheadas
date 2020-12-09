976675891918c9014a7343ecd3bab632f7c19189
rosalybt <rosalybalbuena.15@gmail.com>
2020-12-09 07:22:13 -0400

    feature: crear grilla (de solo una dimension/nivel a la vez) no dinamico

---
+++ index.js
@@ -0,0 +1,41 @@
+
+
+const grilla = document.querySelector(".caja-grilla")
+const items = ['🐯', '🦉', '🦋', '🕷', '🐊', '🦍', '🦜', '🦨', '🦥', '🍄', '🍀'];
+let matriz = [];
+
+const obtenerNumeroAlAzar = (array) => {
+    let numero = Math.floor((Math.random() * array.length));
+    return numero;
+};
+
+
+const obtenerItemAlAzar = (array) => {
+    let fruta = array[obtenerNumeroAlAzar(array)]
+    return fruta;
+};
+
+
+const crearGrilla = (filas, columnas, array) => {
+    const anchoDeGrilla = 500 / columnas
+    grilla.style.width = '500px'
+    grilla.innerHTML = ''
+    matriz = [];
+
+    for (let i = 0; i < filas; i++) {
+        matriz[i] = [];
+        for (let j = 0; j < columnas; j++) {
+            matriz[i][j] = obtenerItemAlAzar(array);
+
+            grilla.innerHTML += `<div id="grilla"  data-fila=${i} data-columna=${j}>
+                              ${matriz[i][j]}
+                              </div>`;
+
+
+        }
+    }
+    return grilla;
+};
+
+
+crearGrilla(9, 9, items);
